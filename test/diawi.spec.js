c427830c8ee8f528d505541f625c3df975e1c4f5
Luiz Esmiralha <esmiralha@br.ibm.com>
2019-02-23 07:31:37 -0300

    more tests

---
+++ test/diawi.spec.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+
+describe('Upload command tests', function() {
+  it('', function() {
+    assert.equal(1, 1);
+  });
+});
--- test/main.spec.js
+++ test/main.spec.js
@@ -1,17 +1,8 @@
 const assert = require('assert');
 const execSync = require('child_process').execSync;
-const SEGUNDOS = 1000;
 
 describe('command-line tests', function() {
-  before(function() {
-    this.timeout(10 * SEGUNDOS);
-    execSync('npm link');
-  });
-
-  after(function() {
-    this.timeout(10000);
-    execSync('npm unlink');
-  });
+  const CLI_LOCATION = './bin/main.js';
 
   beforeEach(function() {
 
@@ -22,33 +13,33 @@ describe('command-line tests', function() {
   });
 
   it('should return semver version number when called with --version', function() {
-    const version = execSync('diawi --version').toString();
+    const version = execSync(CLI_LOCATION + ' --version').toString();
     const semverPattern = /^\d*\.\d*\.\d*\n$/;
     assert(version.toString().match(semverPattern), 'Versão: ' + version.toString());
   });
 
   it('should throw when called with no params', function() {
     assert.throws(() => {
-      execSync('diawi');
+      execSync(CLI_LOCATION);
     });
   });
 
   it('should throw when called with unrecognized param', function() {
     assert.throws(() => {
-      execSync('diawi --qweqwe');
+      execSync(CLI_LOCATION + ' --qweqwe');
     });
   });
 
   it('should throw when called with unrecognized param', function() {
     assert.throws(() => {
-      execSync('diawi --qweqwe');
+      execSync(CLI_LOCATION + ' --qweqwe');
     });
   });
 
   it('should accept a token and a file', function() {
     execSync('touch DIAWI_TEMP_FILE');
     try {
-      execSync('diawi TOKEN DIAWI_TEMP_FILE --dry-run');
+      execSync(CLI_LOCATION + ' TOKEN DIAWI_TEMP_FILE --dry-run');
     } catch (err) {
       assert.fail('Threw exception');
     } finally {
